78b9ecde3c833a30716b3c811c25ff0b00ce8e40
Hiroki <xxyy_z@pb3.so-net.ne.jp>
2024-06-10 12:43:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if st.session_state["messages"]:
 
     st.write(messages);
 
-    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+    for message in reversed(messages[len(messages)-1:]):  # 直近のメッセージを上に
         speaker = "🙂"
         if message["role"]=="assistant":
             speaker="🤖"
