2c2cae3fa0b49fe6ab93cce8a31ecc6a25451b44
Hiroki <xxyy_z@pb3.so-net.ne.jp>
2024-06-10 12:26:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import openai
+from openai import OpenAI
 
-# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+client = OpenAI(
+    # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+    api_key = st.secrets.OpenAIAPI.openai_api_key
+)
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
@@ -23,13 +26,13 @@ def communicate():
     }
     messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages, 
+    completion = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = messages, 
         temperature=st.secrets.AppSettings.temperature
-    )  
+    )
 
-    bot_message = response["choices"][0]["message"]
+    bot_message = completion.choices[0].message.content
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
