a047ed9fc4748a12da458eba663c5132ded21dbd
Hiroki <xxyy_z@pb3.so-net.ne.jp>
2024-06-11 12:28:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ st.write(st.secrets.AppSettings.body)
 st.write(st.secrets.AppSettings.body2)
 st.write(st.secrets.AppSettings.body3)
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1, 4])
 a_url = 'https://www.tokushima-u.ac.jp/fs/2/4/8/2/9/4/_/tokupon_1.jpg'
 col1.image(io.BytesIO(requests.get(a_url).content), caption='とくぽん', width=100) #use_column_width=True)
 
